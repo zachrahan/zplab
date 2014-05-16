8a8bc834467023e6e29578742d710e5b4cb85169
Erik Hvatum <ice.rikh@gmail.com>
2014-05-16 11:07:32 -0500

    Add Device Observers.

--- acquisition/device.py
+++ acquisition/device.py
@@ -8,6 +8,7 @@ class Device:
         self._name = name
         self._typeName = 'Device'
         self._subDevices = []
+        self._observers = set()
 
     def _appendTypeName(self, typeName):
         self._typeName += '.' + typeName
@@ -30,11 +31,35 @@ class Device:
 
     @property
     def subDevices(self):
-        return self._subDevices
+        return self._subDevices.copy()
 
     def _addSubDevice(self, subDevice):
         if subDevice in self.subDevices:
             raise DeviceException(self, '_addSubDevice(self, subDevice): {} is already a sub-device of {}.'.format(self.fancyName, subDevice.fancyName))
+
+    @property
+    def observers(self):
+        return self._observers.copy()
+
+    def observerIsAttached(self, observer):
+        return observer in self._observers
+
+    def attachObserver(self, observer):
+        if observer in self._observers:
+            raise DeviceException(self, 'The specified Observer instance is already in this Device\'s set of Observers.')
+        self._observers.add(observer)
+        # The observer does not necessarily care to know if it was attached.  If we try to tell it that it was and it replies that we should go to heck,
+        # we don't let that get us down.  We continue doing our thing, and we don't even hold a grudge against the observer, even if it was sort of a
+        # jerk to us.  This is our general policy when proferring information to Observer instances and also to tetchy human teenager instances.
+        try:
+            observer.notifyAttached(self)
+        except AttributeError as e:
+            pass
+
+    def detachObserver(self, observer):
+        if observer not in self._observers:
+            raise DeviceException(self, 'The specified Observer instance is not in this Device\'s set of Observers.')
+        self._observers.remove(observer)
 
 class DeviceException(AcquisitionException):
     def __init__(self, device, description):
--- acquisition/lumencor/lumencor.py
+++ acquisition/lumencor/lumencor.py
@@ -231,15 +231,19 @@ class Lumencor(Device):
         '''Apply new lamp states contained in the lampStates argument, which should be a dict of lamp names to LampState objects.  The state of any lamp
         not appearing in the lampStates argument dict is not modified.  So, lumencoreinstance.lampStates = {} is a no-op.  The specified state changes
         are consolidated into the fewest number of serial commands required before being dispatched to the Lumencor hardware.'''
-        from IPython.core.debugger import Tracer; breakpoint1 = Tracer();
+        lampStateChangesForObserver = {}
         power_rgcu = {}
         power_bt = {}
         for lampName, newLampState in lampStates.items():
-            curPower = self._lampStates[lampName].power
+            curLampState = self._lampStates[lampName]
+            curPower = curLampState.power
             newPower = newLampState.power
             if newPower != curPower:
+                lampStateChangesForObserver[lampName] = {'power' : newPower}
+                if curLampState.enabled != newLampState.enabled:
+                    lampStateChangesForObserver[lampName]['enabled'] = newLampState.enabled
                 #if lampName in ['red', 'green', 'cyan', 'uv']:
-                if newLampState.idx in range(4): # Same effect as line above, but faster
+                if newLampState.idx in range(4): # Same effect as line above but faster
                     powerdict = power_rgcu
                 else:
                     powerdict = power_bt
@@ -249,9 +253,10 @@ class Lumencor(Device):
                     lampIdxs.append(newLampState.idx)
                 else:
                     powerdict[newPower] = [newLampState.idx]
-            lampState = self._lampStates[lampName]
-            lampState.enabled = newLampState.enabled
-            lampState.power = newLampState.power
+            elif curLampState.enabled != newLampState.enabled:
+                lampStateChangesForObserver[lampName] = {'enabled' : newLampState.enabled}
+            curLampState.enabled = newLampState.enabled
+            curLampState.power = newLampState.power
 
         def updatePowers(commandBase, powerdict):
             if len(powerdict) > 0:
@@ -265,6 +270,13 @@ class Lumencor(Device):
         updatePowers(Lumencor._lampRgcuPowerCommandBase, power_rgcu)
         updatePowers(Lumencor._lampBtPowerCommandBase, power_bt)
         self._updateDisablement()
+
+        for observer in self._observers:
+            try:
+                observer.notifyLumencorLampStatesChanged(self, lampStateChangesForObserver)
+            except AttributeError as e:
+                pass
+
 
     @property
     def redEnabled(self):
