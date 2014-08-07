0210aeefe40ac00abeaf2647e99c9cbe53ae575c
Erik Hvatum <ice.rikh@gmail.com>
2014-08-07 16:15:18 -0500

    Committing soon-to-be-deleted HDFStore code that may serve as a useful reference in the future.

--- acquisition_scripts/sample.py
+++ acquisition_scripts/sample.py
@@ -1,3 +1,4 @@
+import json
 from PyQt5 import Qt
 import pandas
 from pathlib import Path
@@ -13,8 +14,29 @@ class Experiment(Qt.QObject):
         self.runTimer = Qt.QTimer(self)
         # The index of the most recently completed or currently executing run
         self.runIndex = -1
-        
-        self.wellsFPath = self.path / '{}_wells.csv'.format(self.name)
-        if self.wellsFPath.exists():
-            self.wells = pandas.read_csv(str(self.wellsFPath), escapechar='\\')
-        self.dataFPath = self.path / '{}_data.csv'.format(self.name)
+
+        self.storeFPath = self.path / '{}__wells_and_data.h5'.format(self.name)
+        self.store = pandas.HDFStore(str(self.storeFPath))
+
+        self.lastWellIndex = -1
+        if 'wells' in self.store:
+            self.lastWellIndex = self.store.get_storer('wells').nrows - 1
+
+        self.lastRunIndex = -1
+        if 'data' in self.store:
+            self.lastRunIndex = self.store.get_storer('data').nrows - 1
+
+        self.configFPath = self.path / '{}__config.json'.format(self.name)
+        if self.configFPath.exists():
+            with self.configFPath.
+        else:
+            self.config = {'interval_in_milliseconds'
+
+    def storeWell(self, x, y, z):
+        self.lastWellIndex += 1
+        well = {'well':self.lastWellIndex, 'x':x, 'y':y, 'z':z}
+        self.store.append('wells', pandas.DataFrame((well,)), format='t')
+
+    def storeDataRow(self, imageFPath, x, y, z, fmName, fmValue, type_):
+        self.lastRunIndex += 1
+        self.store.append('data', pandas.DataFrame)
