5313cb96a8118294b143a4cf2066f192256a8a92
Erik Hvatum <ice.rikh@gmail.com>
2014-08-07 17:20:18 -0500

    Continue implementing sample acquisition script.

--- acquisition_scripts/sample.py
+++ acquisition_scripts/sample.py
@@ -6,37 +6,71 @@ from pathlib import Path
 class Experiment(Qt.QObject):
     def __init__(self, deviceTreeRoot, experimentName, experimentDataDirectory, parent=None):
         super().__init__(parent)
+
         self.root = deviceTreeRoot
         self.name = experimentName
         self.path = Path(experimentDataDirectory)
         if not self.path.exists():
             self.path.mkdir(parents=True)
+
         self.runTimer = Qt.QTimer(self)
-        # The index of the most recently completed or currently executing run
-        self.runIndex = -1
-
-        self.storeFPath = self.path / '{}__wells_and_data.h5'.format(self.name)
-        self.store = pandas.HDFStore(str(self.storeFPath))
-
-        self.lastWellIndex = -1
-        if 'wells' in self.store:
-            self.lastWellIndex = self.store.get_storer('wells').nrows - 1
-
-        self.lastRunIndex = -1
-        if 'data' in self.store:
-            self.lastRunIndex = self.store.get_storer('data').nrows - 1
 
         self.configFPath = self.path / '{}__config.json'.format(self.name)
+        self.configSwapTempFPath = self.path / '{}__config.json._'.format(self.name)
+
+        self.checkpointFPath = self.path / '{}__checkpoint.json'.format(self.name)
+        self.checkpointSwapTempFPath = self.path / '{}__checkpoint.json._'.format(self.name)
+
+        self.wells = []
+        self.interval = None
         if self.configFPath.exists():
-            with self.configFPath.
-        else:
-            self.config = {'interval_in_milliseconds'
+            with self.configFPath.open() as configF:
+                config = json.load(configF)
+            self.interval = config['interval']
+            self.wells = config['wells']
 
-    def storeWell(self, x, y, z):
-        self.lastWellIndex += 1
-        well = {'well':self.lastWellIndex, 'x':x, 'y':y, 'z':z}
-        self.store.append('wells', pandas.DataFrame((well,)), format='t')
+        # The index of the most recently completed or currently executing run
+        self.runIndex = -1
+        # The start time of the most recently completed or currently executing run
+        self.runTime = None
+        if self.checkpointFPath.exists():
+            with self.checkpointFPath.open() as checkpointF:
+                checkpoint = json.load(checkpointF)
+            self.runIndex = checkpoint['runIndex']
+            self.runTime = checkpoint['runTime']
 
-    def storeDataRow(self, imageFPath, x, y, z, fmName, fmValue, type_):
-        self.lastRunIndex += 1
-        self.store.append('data', pandas.DataFrame)
+    def writeConfig(self):
+        with self.configSwapTempFPath.open('w') as configSwapTempF:
+            config = {'interval':self.interval,
+                      'wells':self.wells}
+            json.dump(configSwapTempF, config)
+        self.configSwapTempFPath.replace(self.configFPath)
+
+    def writeCheckpoint(self):
+        with self.checkpointSwapTempFPath.open('w') as checkpointSwapTempF:
+            checkpoint = {'runIndex':self.runIndex,
+                          'runTime':self.runTime}
+            json.dump(checkpointSwapTempF, checkpoint)
+        self.checkpointSwapTempFPath.replace(self.checkpointFPath)
+
+    def addWells(self):
+        from acquisition.pedals.pedal import WaitPedal
+        wp = WaitPedal('/dev/ttyACM2')
+            try:
+                while True:
+                    root.dm6000b.objectiveTurret.position = 1
+                    wp.wait('high')
+                    root.dm6000b.objectiveTurret.position = 2
+                    wp.wait('low')
+                    wp.wait('high')
+                    
+                    poss.append(getstage())
+                    wp.wait('low')
+            except KeyboardInterrupt:
+                return poss
+
+    def start(self, interval):
+        pass
+
+    def resume(self):
+        pass
