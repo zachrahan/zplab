571a8c653d3133f9876d2164afa61f515f40e2b9
Erik Hvatum <ice.rikh@gmail.com>
2014-08-29 17:05:41 -0500

    Implement client event thread.

--- zacquisition/service.py
+++ zacquisition/service.py
@@ -25,9 +25,50 @@
 import enum
 import gevent
 import pathlib
+import threading
+import weakref
 import zmq.green as zmq
-from zacquisition.service_property import ServiceProperty
-from zacquisition import service_property_validators as spvs
+#from zacquisition.service_property import ServiceProperty
+#from zacquisition import service_property_validators as spvs
+
+class _ClientEventThread(threading.Thread):
+    _nextId = 0
+    _nextIdLock = threading.Lock()
+
+    def __init__(self, service, eventSocketURI):
+        super().__init__()
+        self._service = weakref.ref(service)
+        self._eventSocketURI = eventSocketURI
+        with self._nextIdLock:
+            id_ = _ClientEventThread._nextId
+            _ClientEventThread._nextId += 1
+        self._stopThreadSocketURI = 'inproc://_ClientEventThread__{}'.format(id_)
+        self._stopThreadSendSocket = self._service()._zc.socket(zmq.PAIR)
+        self._stopThreadSendSocket.bind(self._stopThreadSocketURI)
+
+    def _stopThreadSocketListener(self):
+        '''"_stopThreadSocketListener" in the sense "listen to a stop socket" and not "stop a socket listener".'''
+        while True:
+            s = self._stopThreadReceiveSocket.recv_string()
+            if s == 'stop':
+                self._eventSocketListenerGt.kill()
+                break
+
+    def _eventSocketListener(self):
+        while True:
+            md = self._eventSocket.recv_json()
+
+    def run(self):
+        self._eventSocket = self._service()._zc.socket(zmq.SUB)
+        self._eventSocket.connect(self._eventSocketURI)
+        self._stopThreadReceiveSocket = self._service()._zc.socket(zmq.PAIR)
+        self._stopThreadReceiveSocket.connect(self._stopThreadSocketURI)
+        self._eventSocketListenerGt = gevent.spawn(self._eventSocketListener)
+        self._stopThreadSocketListenerGt = gevent.spawn(self._stopThreadSocketListener)
+        gevent.wait((self._eventSocketListenerGt, self._stopThreadSocketListenerGt))
+
+    def stop(self):
+        self._stopThreadSendSocket.send_string('stop')
 
 class Service:
     class InstanceType(enum.Enum):
@@ -36,18 +77,22 @@ class Service:
 
     ipcSocketPath = ServiceProperty(default=None, name='ipcSocketPath', validators=spvs.readOnly)
     eventIpcSocketFPath = ServiceProperty(default=None, name='eventIpcSocketFPath', validators=spvs.readOnly)
-    reqIpcSocketFPath = ServiceProperty(default=None, name='reqIpcSocketFPath', validators=spvs.readOnly)
+    commandIpcSocketFPath = ServiceProperty(default=None, name='commandIpcSocketFPath', validators=spvs.readOnly)
     eventTcpPortNumber = ServiceProperty(default=None, name='eventTcpPortNumber', validators=spvs.readOnly)
-    reqTcpPortNumber = ServiceProperty(default=None, name='reqTcpPortNumber', validators=spvs.readOnly)
+    commandTcpPortNumber = ServiceProperty(default=None, name='commandTcpPortNumber', validators=spvs.readOnly)
     pyClassString = ServiceProperty(default=None, name='pyClassString', validators=spvs.readOnly)
     # Because service name is used to identify the Unix domain socket (IPC socket), service name can only be set by providing
     # a name argument to __init__(..) and is otherwise read-only.
     name = ServiceProperty(default='UNNAMED SERVICE', name='name', validators=spvs.readOnly)
 
     def __init__(self, pyClassString, zmqContext=None, instanceType=InstanceType.Daemon, parent=None, name=None, \
-                 ipcSocketPath='/tmp/zacquisition', eventTcpPortNumber=51500, reqTcpPortNumber=51501):
-        '''ipcPathPrefix, eventTcpPortNumber, and reqTcpPortNumber are only used if parent is None.  If parent is not None,
-        these values are computed from those of the parent and the values supplied are ignored.'''
+                 ipcSocketPath='/tmp/zacquisition', eventTcpPortNumber=51500, commandTcpPortNumber=51501, \
+                 hostName=None):
+        '''ipcPathPrefix, eventTcpPortNumber, and commandTcpPortNumber are only used if parent is None.
+        If parent is not None, these values are computed from those of the parent and the values supplied
+        are ignored.
+
+        The hostName argument is only used if instanceType is Client.'''
         # Enumerate ServiceProperties so that a list of their names is available via the serviceProperties property
         # (which is itself a standard Python property and not a ServiceProperty descriptor instance).
         self._serviceProperties = set()
@@ -82,55 +127,60 @@ class Service:
         self._instanceType = Service.InstanceType(instanceType)
 
         if self._instanceType == Service.InstanceType.Daemon:
-            self._pubSocket = self._zc.socket(zmq.PUB)
-            self._repSocket = self._zc.socket(zmq.REP)
-            if self._parent is None:
-                ipcsp = pathlib.Path(ipcSocketPath)
-                etcpn = eventTcpPortNumber
-                rtcpn = reqTcpPortNumber
-            else:
-                # Use directory in which parent's IPC sockets reside as IPC path prefix
-                ipcsp = self._parent.ipcSocketPath
-                etcpn = self._parent.eventTcpPortNumber + 2
-                rtcpn = self._parent.reqTcpPortNumber + 2
-            # Place our IPC socket in a subdirectory with our (this service's) name
-            ipcsp /= self.name
-            if not ipcsp.exists():
-                ipcsp.mkdir(parents=True)
-            eipcsfp = ipcsp / (self.name + '__EVENT__.ipc')
-            ripcsfp = ipcsp / (self.name + '__REQUEST__.ipc')
-            Service.ipcSocketPath.setWithoutValidating(self, ipcsp)
-            # Use our name for IPC socket filenames
-            self._pubSocket.bind('ipc://' + str(eipcsfp))
-            self._repSocket.bind('ipc://' + str(ripcsfp))
-            Service.eventIpcSocketFPath.setWithoutValidating(self, eipcsfp)
-            Service.reqIpcSocketFPath.setWithoutValidating(self, ripcsfp)
-            if self._parent is None:
-                # If we are using port numbers supplied directly by the user, then only those ports will do
-                self._pubSocket.bind('tcp://*:{}'.format(etcpn))
-                self._repSocket.bind('tcp://*:{}'.format(rtcpn))
-            else:
-                # If we are using computed port numbers, then we use the first available ports greater than or
-                # equal to desired
-                def openTcpPort(port, number):
-                    while True:
-                        try:
-                            port.bind('tcp://*:{}'.format(number))
-                            break
-                        except zmq.ZMQError:
-                            number += 1
-                            if number >= 65536:
-                                raise RuntimeError('Failed to find available TCP port number.')
-                    return number
-                etcpn = openTcpPort(self._pubSocket, etcpn)
-                rtcpn = openTcpPort(self._repSocket, rtcpn)
-            Service.eventTcpPortNumber.setWithoutValidating(self, etcpn)
-            Service.reqTcpPortNumber.setWithoutValidating(self, rtcpn)
-
-            self._reqListenerGreenlet = gevent.spawn(self._reqListener)
+            self._daemonInit(ipcSocketPath, eventTcpPortNumber, commandTcpPortNumber)
         else:
-            self._reqSocket = self._zc.socket(zmq.REQ)
+            self._clientInit(ipcSocketPath, eventTcpPortNumber, commandTcpPortNumber)
 
+    def _daemonInit(self, ipcSocketPath, eventTcpPortNumber, commandTcpPortNumber):
+        self._pubSocket = self._zc.socket(zmq.PUB)
+        self._repSocket = self._zc.socket(zmq.REP)
+        if self._parent is None:
+            ipcsp = pathlib.Path(ipcSocketPath)
+            etcpn = eventTcpPortNumber
+            rtcpn = commandTcpPortNumber
+        else:
+            # Use directory in which parent's IPC sockets reside as IPC path prefix
+            ipcsp = self._parent.ipcSocketPath
+            etcpn = self._parent.eventTcpPortNumber + 2
+            rtcpn = self._parent.commandTcpPortNumber + 2
+        # Place our IPC socket in a subdirectory with our (this service's) name
+        ipcsp /= self.name
+        if not ipcsp.exists():
+            ipcsp.mkdir(parents=True)
+        eipcsfp = ipcsp / (self.name + '__EVENT__.ipc')
+        ripcsfp = ipcsp / (self.name + '__REQUEST__.ipc')
+        Service.ipcSocketPath.setWithoutValidating(self, ipcsp)
+        # Use our name for IPC socket filenames
+        self._pubSocket.bind('ipc://' + str(eipcsfp))
+        self._repSocket.bind('ipc://' + str(ripcsfp))
+        Service.eventIpcSocketFPath.setWithoutValidating(self, eipcsfp)
+        Service.commandIpcSocketFPath.setWithoutValidating(self, ripcsfp)
+        if self._parent is None:
+            # If we are using port numbers supplied directly by the user, then only those ports will do
+            self._pubSocket.bind('tcp://*:{}'.format(etcpn))
+            self._repSocket.bind('tcp://*:{}'.format(rtcpn))
+        else:
+            # If we are using computed port numbers, then we use the first available ports greater than or
+            # equal to desired
+            def openTcpPort(port, number):
+                while True:
+                    try:
+                        port.bind('tcp://*:{}'.format(number))
+                        break
+                    except zmq.ZMQError:
+                        number += 1
+                        if number >= 65536:
+                            raise RuntimeError('Failed to find available TCP port number.')
+                return number
+            etcpn = openTcpPort(self._pubSocket, etcpn)
+            rtcpn = openTcpPort(self._repSocket, rtcpn)
+        Service.eventTcpPortNumber.setWithoutValidating(self, etcpn)
+        Service.commandTcpPortNumber.setWithoutValidating(self, rtcpn)
+
+        self._reqListenerGreenlet = gevent.spawn(self._reqListener)
+
+    def _clientInit(self, ipcSocketPath, eventTcpPortNumber, commandTcpPortNumber):
+        self._reqSocket = self._zc.socket(zmq.REQ)
 
     def describeRecursive(self):
         if self.instanceType == self.InstanceType.Client:
@@ -142,9 +192,9 @@ class Service:
             'pyClassString':self.pyClassString,
             'name':self.name,
             'eventIpcSocketFPath':str(self.eventIpcSocketFPath),
-            'reqIpcSocketFPath':str(self.reqIpcSocketFPath),
+            'commandIpcSocketFPath':str(self.commandIpcSocketFPath),
             'eventTcpPortNumber':self.eventTcpPortNumber,
-            'reqTcpPortNumber':self.reqTcpPortNumber
+            'commandTcpPortNumber':self.commandTcpPortNumber
 
     def _reqListener(self):
         '''Intended to run in a greenlet.'''
