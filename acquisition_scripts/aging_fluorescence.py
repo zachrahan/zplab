a72525f19493b6e466f430760cd231cb6fe6d89b
Erik Hvatum <ice.rikh@gmail.com>
2014-09-23 15:58:06 -0500

    Fixes

--- acquisition_scripts/aging_fluorescence.py
+++ acquisition_scripts/aging_fluorescence.py
@@ -24,21 +24,32 @@ class AgingFluorescence(Qt.QObject):
                 self.positions_control, self.positions_exp = json.load(f)
 
     def get_more_positions(self):
-        self.stop_button = Qt.QPushButton('stop getting positions')
-        self.stop_button.clicked.connect(self.stop_getting_positions)
-        self.stop_button.show()
+        if hasattr(self, 'pos_get_dialog'):
+            raise RuntimeError('get_more_positions() already in progress...')
+        self.pos_get_dialog = Qt.QDialog()
+        self.pos_get_dialog.setWindowTitle('Getting positions...')
+        self.pos_get_dialog.setLayout(Qt.QVBoxLayout())
+        self.pos_get_dialog.stop_button = Qt.QPushButton('stop getting positions')
+        self.pos_get_dialog.layout().addWidget(self.pos_get_dialog.stop_button)
+        self.pos_get_dialog.posSetLabel = Qt.QLabel('control')
+        self.pos_get_dialog.layout().addWidget(self.pos_get_dialog.posSetLabel)
+        self.pos_get_dialog.stop_button.clicked.connect(self.stop_getting_positions)
         self.root.pedals.pedalUpChanged.connect(self.on_pedal_up)
+        self.pos_get_dialog.show()
 
     def on_pedal_up(self, pedal, isUp):
         if not isUp:
-            pos = (self.root.dm6000b.stageX.pos, self.root.dm6000b.stageY.pos, self.root.dm6000b.stageZ.pos)
-            pl = self.positions_control if pedal == 0 else self.positions_exp
-            pl.append(pos)
+            if pedal == 0:
+                pos = (self.root.dm6000b.stageX.pos, self.root.dm6000b.stageY.pos, self.root.dm6000b.stageZ.pos)
+                pl = self.positions_control if self.pos_get_dialog.posSetLabel.text() == 'control' else self.positions_exp
+                pl.append(pos)
+            elif pedal == 1:
+                self.pos_get_dialog.posSetLabel.setText('control' if self.pos_get_dialog.posSetLabel.text() == 'exp' else 'exp')
 
     def stop_getting_positions(self):
         self.root.pedals.pedalUpChanged.disconnect(self.on_pedal_up)
-        self.stop_button.close()
-        del self.stop_button
+        self.pos_get_dialog.close()
+        del self.pos_get_dialog
 
     def save_positions(self):
         with open(str(self.positions_fpath), 'w') as f:
@@ -55,11 +66,12 @@ class AgingFluorescence(Qt.QObject):
         else:
             raise ValueError('filterSetNumber must be 0 or 1')
         time.sleep(1)
-        ## NEED TO EXPLICITLY SET EXPOSURE TIMES -- 20 ms for brightfield and 150 ms for fluorescence
         for positionSet in ('control', 'exp'):
-            positions = self.positions_control if positionSet is 'control' else self.positions_exp
+            positions = self.positions_control if positionSet == 'control' else self.positions_exp
             for positionIndex, position in enumerate(positions):
+                print('posindex: {}, position: {}'.format(positionIndex, position))
                 self.root.dm6000b.stageX.pos, self.root.dm6000b.stageY.pos, self.root.dm6000b.stageZ.pos = position
+                self.root.dm6000b.waitForReady()
                 self.root.brightfieldLed.enabled = True
                 self.root.brightfieldLed.power = 200
                 self.root.camera._camera.AT_Flush()
@@ -67,55 +79,56 @@ class AgingFluorescence(Qt.QObject):
                 self.root.camera.shutter = self.root.camera.Shutter.Rolling
                 self.root.camera.triggerMode = self.root.camera.TriggerMode.Software
                 self.root.camera.cycleMode = self.root.camera.CycleMode.Fixed
+                # 20ms brightfield exposure time
+                self.root.camera.exposureTime = 0.020
                 self.root.camera.frameCount = imageCount
                 for buffer in buffers:
                     self.root.camera._camera.AT_QueueBuffer(buffer)
                 self.root.camera._camera.AT_Command(self.root.camera.Feature.AcquisitionStart)
                 time.sleep(0.08)
+                self.root.camera.commandSoftwareTrigger()
+                time.sleep(0.040)
+                self.root.brightfieldLed.enabled = False
+                # 150ms fluorescence exposure time
+                self.root.camera.exposureTime = 0.150
                 if filterSetNumber == 0:
-                    self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
-                    self.root.brightfieldLed.enabled = False
                     self.root.lumencor.UVEnabled = True
                     self.root.lumencor.UVPower = 255
                     time.sleep(0.08)
                     self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
+                    time.sleep(0.170)
                     self.root.lumencor.UVEnabled = False
                     self.root.lumencor.cyanEnabled = True
                     self.root.lumencor.cyanPower = 255
                     time.sleep(0.08)
                     self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
+                    time.sleep(0.170)
                     self.root.lumencor.cyanEnabled = False
                     self.root.lumencor.greenEnabled = True
                     self.root.lumencor.greenPower = 255
                     time.sleep(0.08)
                     self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
+                    time.sleep(0.170)
                     self.root.lumencor.disable()
                     names = ['bf-DFTr', 'DAPI', 'FITC', 'TRITC']
                 elif filterSetNumber == 1:
-                    self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
-                    self.root.brightfieldLed.enabled = False
                     self.root.lumencor.blueEnabled = True
                     self.root.lumencor.bluePower = 255
                     time.sleep(0.08)
                     self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
+                    time.sleep(0.170)
                     self.root.lumencor.blueEnabled = False
                     self.root.lumencor.tealEnabled = True
                     self.root.lumencor.tealPower = 255
                     time.sleep(0.08)
                     self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
+                    time.sleep(0.170)
                     self.root.lumencor.tealEnabled = False
                     self.root.lumencor.greenEnabled = True
                     self.root.lumencor.greenPower = 255
                     time.sleep(0.08)
                     self.root.camera.commandSoftwareTrigger()
-                    time.sleep(0.020)
+                    time.sleep(0.170)
                     self.root.lumencor.disable()
                     names = ['bf-CYmC', 'CFP', 'YFP', 'mCherry']
                 for i in range(imageCount):
@@ -126,60 +139,3 @@ class AgingFluorescence(Qt.QObject):
                     if rw is not None:
                         rw.showImage(buffers[i])
                     skio.imsave(str(imFP), buffers[i])
-
-#   def executeRun(self):
-#       self.root.dm6000b.lamp.ilShutterOpened = True
-#       self.root.dm6000b.lamp.tlShutterOpened = True
-#       runStartTime = time.time()
-#       self.root.brightfieldLed.enabled = True
-#       self.root.brightfieldLed.power = 255
-#       self.root.camera._camera.AT_Flush()
-#       buffers = [self.root.camera.makeAcquisitionBuffer() for i in range(4)]
-#       self.root.camera.shutter = self.root.camera.Shutter.Rolling
-#       self.root.camera.triggerMode = self.root.camera.TriggerMode.Software
-#       self.root.camera.cycleMode = self.root.camera.CycleMode.Fixed
-#       self.root.camera.frameCount = 4
-#       for buffer in buffers:
-#           self.root.camera._camera.AT_QueueBuffer(buffer)
-#       self.root.camera._camera.AT_Command(self.root.camera.Feature.AcquisitionStart)
-#       time.sleep(0.08)
-#       self.root.camera.commandSoftwareTrigger()
-#       time.sleep(0.020)
-#       self.root.brightfieldLed.enabled = False
-#       self.root.lumencor.UVEnabled = True
-#       self.root.lumencor.UVPower = 255
-#       time.sleep(0.08)
-#       self.root.camera.commandSoftwareTrigger()
-#       time.sleep(0.020)
-#       self.root.lumencor.UVEnabled = False
-#       self.root.lumencor.cyanEnabled = True
-#       self.root.lumencor.cyanPower = 255
-#       time.sleep(0.08)
-#       self.root.camera.commandSoftwareTrigger()
-#       time.sleep(0.020)
-#       self.root.lumencor.cyanEnabled = False
-#       self.root.lumencor.greenEnabled = True
-#       self.root.lumencor.greenPower = 255
-#       time.sleep(0.08)
-#       self.root.camera.commandSoftwareTrigger()
-#       time.sleep(0.020)
-#       self.root.lumencor.disable()
-#       for i in range(4):
-#           print(i)
-#           self.root.camera._camera.AT_WaitBuffer(1000)
-#       self.root.camera._camera.AT_Command(self.root.camera.Feature.AcquisitionStop)
-#
-#       self.saveImage(buffers[0], 'bf', runStartTime)
-#       self.saveImage(buffers[1], 'uv', runStartTime)
-#       self.saveImage(buffers[2], 'cyan', runStartTime)
-#       self.saveImage(buffers[3], 'greenyellow', runStartTime)
-#
-#       self.currRun += 1
-#       if self.runCount is not None and self.currRun >= self.runCount:
-#           self.runTimer.stop()
-#
-#   def saveImage(self, image, type_, time_):
-#       imFP = self.outPath / '{}_{}_{}.png'.format(self.imagePrefix, type_, time_)
-#       if self.rw is not None:
-#           self.rw.showImage(image)
-#       skio.imsave(str(imFP), image)
