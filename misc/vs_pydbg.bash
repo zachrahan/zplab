bc509b3630eb65eed74f0ab0ab78e285f99e044c
Erik Hvatum <ice.rikh@gmail.com>
2014-10-17 14:10:02 -0500

    Use duck typing rather than name sniffing to distinguish child async devices.

--- misc/vs_pydbg.bash
+++ misc/vs_pydbg.bash
@@ -6,5 +6,5 @@ unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
     python3.4 /Applications/SlickEdit2013.app/Contents/resource/tools/pydbgp-1.1.0-1/bin/py3_dbgp.py -d 127.0.0.1:${args[0]} -k slickedit ${args[@]:1}
 else
-    python ~ehvatum/slickedit/resource/tools/pydbgp-1.1.0-1/bin/py3_dbgp.py -d 127.0.0.1:${args[0]} -k slickedit ${args[@]:1}
+    python ~ehvatum/slickedit19/resource/tools/pydbgp-1.1.0-1/bin/py3_dbgp.py -d 127.0.0.1:${args[0]} -k slickedit ${args[@]:1}
 fi
