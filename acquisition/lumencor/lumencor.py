96cf1180080dc4505692bfe9d5f5a132d16446f5
Erik Hvatum <ice.rikh@gmail.com>
2014-11-04 17:24:24 -0600

    Accommodate /dev/ttySptrx -> /dev/ttySpectraX name change.

--- acquisition/lumencor/lumencor.py
+++ acquisition/lumencor/lumencor.py
@@ -58,7 +58,7 @@ class Lumencor(Device):
     # Argument contains a dict of dicts: {"lampname" : {"propertyname" : newvalue}}
     lampStatesChanged = QtCore.pyqtSignal(dict)
 
-    def __init__(self, parent=None, deviceName='Lumencor Spectra-X', serialPortDescriptor='/dev/ttySptrx'):
+    def __init__(self, parent=None, deviceName='Lumencor Spectra-X', serialPortDescriptor='/dev/ttySpectraX'):
         super().__init__(parent, deviceName)
         self._serialPort = serial.Serial(serialPortDescriptor, 9600, timeout=0.2)
         if not self._serialPort.isOpen():
