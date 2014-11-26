98200c658d6c521aa72d732bd34a7bf611148966
Erik Hvatum <ice.rikh@gmail.com>
2014-11-26 17:51:14 -0600

    Add optional classifier edge detection preprocessing step.

--- misc/worm_classifier_for_autofocus.py
+++ misc/worm_classifier_for_autofocus.py
@@ -30,6 +30,7 @@ from pathlib import Path
 import scipy.ndimage
 import scipy.ndimage.morphology
 import skimage.exposure
+import skimage.filter
 import skimage.measure
 import skimage.morphology
 import skimage.io as skio
@@ -43,12 +44,28 @@ import os
 import pickle
 import sys
 
+def classify_blockwise(sample_box_width, im, classify_one):
+    sbw = sample_box_width
+    imf = skimage.exposure.equalize_adapthist(im).astype(numpy.float64)
+    ybc = int(imf.shape[0] / sbw)
+    xbc = int(imf.shape[1] / sbw)
+    b = 0
+    bc = ybc * xbc
+    labels = numpy.zeros((ybc, xbc), dtype=numpy.bool)
+    for yb, y in zip(range(ybc), range(0, imf.shape[0], sbw)):
+        for xb, x in zip(range(xbc), range(0, imf.shape[1], sbw)):
+             labels[yb, xb] = classify_one(imf[y:y+sbw, x:x+sbw].ravel()) != 0
+             b += 1
+             print('{}%'.format(100 * b / bc))
+    return labels
+
 class Classifier:
     def __init__(self,
                  liblinear_classifier_object_or_model_fpath,
                  sample_box_width,
                  mask_object_or_fpath=Path(os.path.expanduser('~')) / 'Data' / 'experiment01_a' / 'supplementary_out_exclusion_mask.png',
-                 max_accepted_sample_box_masked_portion=0.25):
+                 max_accepted_sample_box_masked_portion=0.25,
+                 edge_detect=False):
 
         if issubclass(type(liblinear_classifier_object_or_model_fpath), (str, Path)):
             self._linear_classifier = LinearClassifier()
@@ -65,10 +82,13 @@ class Classifier:
             self._exclusion_mask = mask_object_or_fpath > 0
         self._block_exclusion_mask = skimage.measure.block_reduce(self._exclusion_mask, (self._sample_box_width, self._sample_box_width), numpy.sum)
 
-        self._max_masked = max_accepted_sample_masked_portion
+        self._max_masked = max_accepted_sample_box_masked_portion
+        self._edge_detect = edge_detect
 
     def classify_blockwise(self, im):
         imf = skimage.exposure.equalize_adapthist(im).astype(numpy.float64)
+        if self._edge_detect:
+            imf = skimage.filter.edges.sobel(imf)
         ybc = int(imf.shape[0] / self._sample_box_width)
         xbc = int(imf.shape[1] / self._sample_box_width)
         vectors = numpy.empty((ybc * xbc, self._vector_len), dtype=numpy.float64)
@@ -81,7 +101,7 @@ class Classifier:
         return self._linear_classifier.classify(vectors).reshape(ybc, xbc)
 
     def classify_blockwise_and_overlay(self, im, mask_alpha=0.333333):
-        mask = self.classify(im) > 0
+        mask = self.classify_blockwise(im) > 0
         mask = numpy.repeat(mask, self._sample_box_width, axis=0)
         mask = numpy.repeat(mask, self._sample_box_width, axis=1)
         mask = numpy.pad(mask, ((0, im.shape[0]-mask.shape[0]), (0, im.shape[1]-mask.shape[1])), 'constant', constant_values=0)
@@ -89,4 +109,13 @@ class Classifier:
 
     def classify(self, im):
         imf = skimage.exposure.equalize_adapthist(im).astype(numpy.float64)
-
+        if self._edge_detect:
+            imf = skimage.filter.edges.sobel(imf)
+        sbw = self._sample_box_width
+        low_offset = int(sbw / 2)
+        high_offset = sbw - low_offset
+        mask = numpy.zeros((imf.shape[0], imf.shape[1]), dtype=numpy.bool)
+        for y in range(imf.shape[0] - sbw):
+            for x in range(imf.shape[1] - sbw):
+                mask[y+low_offset, x+low_offset] = self._linear_classifier.classify_one_vector(imf[y:y+sbw, x:x+sbw].ravel()) > 0
+        return mask
