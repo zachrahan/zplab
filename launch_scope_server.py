e625bead957f572c0958d011170772a294f09121
Erik Hvatum <ice.rikh@gmail.com>
2014-12-11 12:11:23 -0600

    Remove long-past-expiration-date testing and development scripts. Minor bug fixes for continuous autofocuser with background processing thread.

--- launch_scope_server.py
+++ launch_scope_server.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.4
+# This script simply serves as a debugging target for Python debuggers that are able to launch a script but not
+# a module in the python -m fashion.
 
 from rpc_acquisition import scope_server
-scope_server.server_main(verbose=False)
+scope_server.server_main(verbose=True)
