f752f8b94d0e69191dd961be92d31129576bf295
Erik Hvatum <ice.rikh@gmail.com>
2015-02-16 14:54:44 -0600

    Add Zach's freeimage.py. Update ris_widget debug convenience launcher.

--- launch_riswidget_for_debugging.py
+++ launch_riswidget_for_debugging.py
@@ -4,14 +4,17 @@ for function in ('imread', 'imsave', 'imread_collection'):
     skio.use_plugin('freeimage', function)
 from PyQt5 import Qt
 from ris_widget.ris_widget import RisWidget
+from ris_widget import freeimage
 import sys
 
-#im = skio.imread('/Volumes/scopearray/pharyngeal_pumping_max_fps/pharyngeal_pumping_max_fps_010_0.274411275.png')
-im = skio.imread('/Users/ehvatum/heic1015a.jpg')
+im = skio.imread('/Volumes/scopearray/pharyngeal_pumping_max_fps/pharyngeal_pumping_max_fps_010_0.274411275.png')
+#im = skio.imread('/Users/ehvatum/heic1015a.jpg')
+#im = freeimage.read('/Users/ehvatum/heic1015a.jpg')
+#im = freeimage.read('/Users/ehvatum/zplrepo/ris_widget/top_left.png')
 
 argv = sys.argv
 app = Qt.QApplication(argv)
 rw = RisWidget()
 rw.show()
-rw.show_image(im)
+rw.image_data = im
 app.exec_()
