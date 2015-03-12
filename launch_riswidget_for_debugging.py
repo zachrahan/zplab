9e37c9b8978de59a2322544134d7ec111f289ae1
Erik Hvatum <ice.rikh@gmail.com>
2015-03-12 18:15:37 -0500

    Make forwarding of events to viewport overlay items somewhat work.

--- launch_riswidget_for_debugging.py
+++ launch_riswidget_for_debugging.py
@@ -15,4 +15,25 @@ app = Qt.QApplication(argv)
 rw = RisWidget()
 rw.show()
 rw.image_data = im
+
+#form_scene = rw.image_scene
+form_scene = rw.image_view_overlay_scene
+
+text_edit = form_scene.addWidget(Qt.QTextEdit())
+button = form_scene.addWidget(Qt.QPushButton('push me'))
+button.setStyle(Qt.QStyleFactory.create('Fusion'))
+button.setOpacity(0.5)
+layout = Qt.QGraphicsGridLayout()
+layout.addItem(text_edit, 0, 0)
+layout.addItem(button, 0, 1)
+form = Qt.QGraphicsWidget()
+form.setLayout(layout)
+form_scene.addItem(form)
+text_edit.widget().setText('foo bar baz')
+
+if form_scene is rw.image_view_overlay_scene:
+    rw.image_scene.installEventFilter(rw.image_view_overlay_scene)
+
+del form_scene
+
 app.exec_()
