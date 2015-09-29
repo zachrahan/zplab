15f5cdf2f00d312fe5f681b3c9d625df0b527549
Erik Hvatum <ice.rikh@gmail.com>
2015-09-28 19:27:31 -0500

    Subscribe to and handle objective turret position change events. Implement objective widget.

--- launch_scope_server.py
+++ launch_scope_server.py
@@ -2,5 +2,5 @@
 # This script simply serves as a debugging target for Python debuggers that are able to launch a script but not
 # a module in the python -m fashion.
 
-from rpc_acquisition import scope_server
-scope_server.server_main(verbose=True)
+from rpc_acquisition.scope import scope_server
+scope_server.simple_server_main('*', verbose=True)
