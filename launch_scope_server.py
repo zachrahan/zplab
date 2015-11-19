30c826c0f19b878baa7e9adabdd382368454ac6b
Erik Hvatum <ice.rikh@gmail.com>
2015-11-19 15:07:50 -0600

    Rename LiveWidget to ScopeViewer. Refactor ScopeViewer to wrapper/.qt_object style. Remove peripherally related stuff from ScopeViewer.

--- launch_scope_server.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3.4
-# This script simply serves as a debugging target for Python debuggers that are able to launch a script but not
-# a module in the python -m fashion.
-
-from rpc_acquisition.scope import scope_server
-scope_server.simple_server_main('*', verbose=True)
